086e1e4a9c3292169f2c5cfe1c87f9e103a5b0fb
Charles <charles0527@hotmail.com>
2022-03-25 00:55:25 +0800

    feat: create view folder

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const User = require('./models/users.js')
 const bodyParser = require('body-parser')
+const session = require('express-session')
+
+const port = 3000
+const app = express()
+
+const routes = require('./routes')
+require('./config/mongoose')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
