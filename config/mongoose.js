03835ac2fdf06184118345a8b18f418d9652ca4b
Charles <charles0527@hotmail.com>
2022-03-27 23:34:57 +0800

    refactor: routes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/login_system')
+mongoose.connect('mongodb://localhost/login-system')
 
 const db = mongoose.connection
 
 db.on('error', () => {
-    console.log('mongodb error')
+  console.log('mongodb error!')
 })
 
 db.once('open', () => {
-    console.log('mongodb connected')
+  console.log('mongodb connected!')
 })
 
 module.exports = db
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,51 +1,53 @@
-const express = require('express');
-const router = express.Router();
-const User = require('../../models/accounts');
-const session = require('express-session');
+const express = require('express')
+const router = express.Router()
+const User = require('../../models/accounts')
+const session = require('express-session')
 
 router.get('/', (req, res) => {
-  const id = req.session.userId;
-  return User.findById(id)
-    .lean()
-    .then((user) => {
-      if (id) {
-        console.log('authenticated');
-        return res.render('welcome', { user });
-      } else {
-        console.log('not authenticated');
-        return res.render('index');
-      }
-    });
-});
+    const id = req.session.userId
+    return User.findById(id)
+        .lean()
+        .then(user => {
+            if (id) {
+                console.log('authenticated')
+                return res.render('welcome', { user })
+            } else {
+                console.log('not authenticated')
+                return res.render('index')
+            }
+        })
+})
 
 router.post('/', (req, res) => {
-  const { email, password } = req.body;
-  email.trim().toLowerCase();
+    const { email, password } = req.body
+    email.trim().toLowerCase()
 
-  if (!email || !password) {
-    return res.redirect('/');
-  }
-  return User.findOne({ email })
-    .lean()
-    .then((user) => {
-      const error = !user ? true : password !== user.password;
-      const wrongMsg = !user ? 'Email' : 'Password';
-      if (error) {
-        return res.render('index', { email, password, error, wrongMsg });
-      } else {
-        req.session.userId = user._id;
-        return res.redirect('/');
-      }
-    });
-});
+    if (!email || !password) { return res.redirect('/') }
+    return User.findOne({ email })
+        .lean()
+        .then(user => {
+            const error = !user ? true : password !== user.password
+            const wrongMsg = !user ? 'Email' : 'Password'
+            if (error) {
+                return res.render('index', { email, password, error, wrongMsg })
+            } else {
+                req.session.userId = user._id
+                return res.redirect('/')
+            }
+        })
+})
 
 router.post('/signout', (req, res) => {
-  const customTimeout = 5000;
-  req.session.destroy();
-  console.log('session destroyed');
-  res.setTimeout(customTimeout, () => {
-    res.redirect('/');
-  });
-});
+    const customTimeout = 5000;
+    req.session.destroy()
+    console.log('session destroyed')
+    res.setTimeout(customTimeout, () => {
+        res.redirect('/')
+    })
+})
 
-module.exports = router;
+
+
+
+
+module.exports = router
